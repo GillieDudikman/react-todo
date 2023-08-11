be61f089f84ca480a5e9a6410b2a50dedf8f80df
Gillie Dudikman <gilliedudik@gmail.com>
2023-08-11 17:40:16 +0300

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -2,7 +2,7 @@ const AddTodo = ({getInput, handleChange, input}) => {
     return (
         <div className="topSection">
             <input type="text" value={input} onChange={(e) =>
-                handleChange(e)} placeholder="Add a new do"/>
+                handleChange(e)} onKeyDown={(e) => e.key=="Enter"? handleChange(e)} placeholder="Add a new do"/>
             <button disabled={input.trim() === ""} onClick={getInput}>Add</button>
         </div>
     )
